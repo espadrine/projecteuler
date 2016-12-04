50cf85ef0f51227bffcdfbbf4984619cc33b3248
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-12-04 21:38:30 +0100

    Clean up 4

--- 4.cc
+++ 4.cc
@@ -3,7 +3,6 @@
 
 bool isPalindrome(unsigned long num) {
   std::string digits = std::to_string(num);
-  // Forward and backward iterators.
   size_t len = digits.size();
   for (int i = 0; i < len; i++) {
     if (digits[i] != digits[len - i - 1]) { return false; }
@@ -13,23 +12,20 @@ bool isPalindrome(unsigned long num) {
 
 int main() {
   const unsigned long LARGEST_3_DIGIT = 999, SMALLEST_3_DIGIT = 100;
+  unsigned long largestNum = 0;
   unsigned long n1 = LARGEST_3_DIGIT, n2 = LARGEST_3_DIGIT;
-  unsigned long largestNum = 0,
-                largestN1 = SMALLEST_3_DIGIT, largestN2 = SMALLEST_3_DIGIT;
   for (unsigned long n1 = LARGEST_3_DIGIT; n1 >= SMALLEST_3_DIGIT; n1--) {
     for (unsigned long n2 = LARGEST_3_DIGIT; n2 >= SMALLEST_3_DIGIT; n2--) {
       unsigned long num = n1 * n2;
       if (isPalindrome(num)) {
         if (num > largestNum) {
           largestNum = num;
-          largestN1 = n1;
-          largestN2 = n2;
         }
       }
     }
   }
   if (isPalindrome(largestNum)) {
-    std::cout << largestNum << " = " << largestN1 << " " << largestN2 << std::endl;
+    std::cout << largestNum << std::endl;
   }
   return 0;
 }
